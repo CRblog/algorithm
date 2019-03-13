ac14ab6f7ecabc35d73d5957d40a38a724ca1bee
CRblog <1422423453@qq.com>
2019-03-13 12:26:41 +0800

    111

--- practice/dp/DP_杭电2546.cpp
+++ practice/dp/DP_杭电2546.cpp
@@ -17,15 +17,18 @@ int main()
     while(cin>>n&&n!=0)
     {
     	memset(dp,0,sizeof(dp));
+		//数组初始化
     	for(int i=0;i<n;i++)
 		{
 			cin>>a[i];
 		}
 		sort(a,a+n);
+		//顺序排序，目的是找出最大价值的物品
 		cin>>m;
 		if(m<5)
 		{
 			cout<<m<<endl;
+			//小于5元时无法购买，自然就是当前值
 		}
 		else
 		{
@@ -34,10 +37,14 @@ int main()
 			{
 				for(int j=m-5;j>=a[i];j--)
 				{
-					dp[j]=max(dp[j],dp[j-a[i]]+a[i]);
+					dp[j]=max(dp[j],dp[j-a[i]]+a[i]);//状态方程
 				}
 			}
 			cout<<m-dp[m-5]-a[n-1]<<endl;
+			//背包中能放的最大值就是dp[m-5]
+			//总数是m
+			//最终的值就是总数减去背包中的值，
+			//再减去之前排序找出的那个最大价值的物品
 	   }
 
 	}
