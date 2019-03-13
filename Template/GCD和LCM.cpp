5bf8175b24fb9370a896e863f735273fba87cff7
CRblog <1422423453@qq.com>
2019-03-13 13:03:53 +0800

    算法优化

---
+++ Template/GCD和LCM.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#include<cstdio>
+#include<iostream>
+#include<cstdlib>
+#include<algorithm>
+#include<cmath>
+#include<cstring>
+#include<ctime>
+using namespace std;
+int gcd(int a,int b)//最大公约数优化算法
+{
+	return b==0?a:gcd(b,a%b);
+}
+int lcm(int a,int b)//最小公倍数
+
+{
+	return a/gcd(a,b)*b;
+}
+
+
+int main()
+{
+	cout<<gcd(5,7)<<endl;
+	cout<<lcm(2,5)<<endl;
+	return 0;
+}
