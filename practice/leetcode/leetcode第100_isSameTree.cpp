114cc79fedee1aa739bd9ca2d48223916b3e45d2
CRblog <1422423453@qq.com>
2019-04-03 18:11:24 +0800

    add(leetcode/leetcode第100_isSameTree.cpp leetcode/leetcode第104树的最大深度.cpp)

---
+++ practice/leetcode/leetcode第100_isSameTree.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+struct TreeNode{
+  int val;
+  struct TreeNode *left;
+  struct TreeNode *right;
+};
+bool isSameTree(struct TreeNode* p,struct TreeNode* q){
+
+  if(p==NULL&&q==NULL) {
+    return true;
+  }
+  if(p == NULL||q == NULL)  {
+    return false;
+  }
+  return p->val == q->val 
+    &&isSameTree(p->left,q->left)
+    &&isSameTree(p->right,q->right);
+}
