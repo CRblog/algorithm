2009bb7f608a0ee75d2f774ad579636dcf1eafb3
CRblog <1422423453@qq.com>
2019-04-09 22:54:21 +0800

    add

---
+++ Template/头文件.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#include<cstdio>      //stdio.h
+#include<cstring>     //字符串
+#include<algorithm>   //算法
+#include<iostream>
+#include<string>
+#include<vector>      //容器
+#include<stack>      //栈
+
+#include<bitset>
+//二进制的操作
+//bitset<100000> b；
+//b<<1;//b整体左移
+//b|=10; //b或 1010
+
+#include<cstdlib>    //内储存分配函数等
+#include<cmath>    
+//pow(base, power) 幂函数  sqrt(x) 计算x的平方根   cbrt(x) 计算x的立方根
+//ceil(x) x上取整函数    floor(x) x的下取整函数
+//fmod(y, x) y/x的余数   round(x) x的四舍五入值
+//fabs(x) x的绝对值函数  abs(x) x的绝对值
+//fmax(x, y) 两个参数中的最大值   fmin(x, y) 两个参数中的最小值
+#include<set>
+//set.insert()   set.find()
+//begin() 　　 ,返回set容器的第一个元素
+//end()　　 ,返回set容器的最后一个元素
+//clear() 　　 删除set容器中的所有的元素
+//empty() 　　　判断set容器是否为空
+//max_size() 　 ,返回set容器可能包含的元素最大个数
+//size()　　　　 ,返回当前set容器中的元素个数
+//rbegin　　　 ,返回的值和end()相同
+//rend()　　　　 ,返回的值和rbegin()相同
+#include<list>   //链表
+#include<deque>  //队列
+#include<map>    //关联容器
+//begin()          返回指向map头部的迭代器
+//clear(）         删除所有元素
+//count()          返回指定元素出现的次数
+//empty()          如果map为空则返回true
+//end()            返回指向map末尾的迭代器
+//equal_range()    返回特殊条目的迭代器对
+//erase()          删除一个元素
+//find()           查找一个元素
+//get_allocator()  返回map的配置器
+//insert()         插入元素
+//key_comp()       返回比较元素key的函数
+//lower_bound()    返回键值>=给定元素的第一个位置
+//max_size()       返回可以容纳的最大元素个数
+//rbegin()         返回一个指向map尾部的逆向迭代器
+//rend()           返回一个指向map头部的逆向迭代器
+//size()           返回map中元素的个数
+//swap()            交换两个map
+//upper_bound()     返回键值>给定元素的第一个位置
+//value_comp()      返回比较元素value的函数
+#include<queue>   //队列容器
+using namespace std;
