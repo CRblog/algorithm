0053dfdf1bbb408eb75f664bd349044d296b9749
CRblog <1422423453@qq.com>
2019-04-09 22:50:58 +0800

    add(层序遍历.cpp)

---
+++ practice/层序遍历.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	if(root ==NULL)
+	{
+	   printf("\n");
+	}
+	queue<Node *> 	q;
+	q.push(root);
+	while(!q.empty())
+	{
+	Node *front = q.front();
+	q.pop();
+	printf("%c",front->value);
+	if(front->left!=NULL)
+	{	
+	q.push(front->left);
+	}
+	if(front->right!=NULL)
+	{
+	q.push(front->right);
+	
+	}
+   	return 0;
+}
