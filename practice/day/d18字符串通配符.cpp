b09f951a1dbe0768a45e084b2a9b788089fc3908
CRblog <1422423453@qq.com>
2019-06-13 23:36:19 +0800

    18

---
+++ practice/day/d18字符串通配符.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+bool match(const char* pattern, const char *str)
+{
+//当前字符结束，返回true
+if (*pattern == '\0' && *str == '\0')
+return true;
+//两个字符串有一个先结束，则返回false
+if (*pattern == '\0' || *str == '\0')
+return false;
+if (*pattern == '?')
+{
+//遇到?号，匹配一个字符，跳过一个位置
+return match(pattern + 1, str + 1);
+}
+else if (*pattern == '*')
+{
+// 遇到*号，匹配0个(str不挪动)，1个（两者都向前挪动一个字符）或多个(str向前挪动一个字符)
+return match(pattern + 1, str) || match(pattern + 1, str + 1) || match(pattern, str + 1);
+}
+else if (*pattern == *str)
+{
+//如果当前字符相等，匹配下一个字符
+return match(pattern + 1, str + 1);
+}
+return false;
+}
+int main()
+{
+string pattern, str;
+while (cin >> pattern >> str)
+{
+	bool ret = match(pattern.c_str(), str.c_str());
+	if (ret)
+		cout << "true" << endl;
+	else
+		cout << "false" << endl;
+}
+return 0;
+}
